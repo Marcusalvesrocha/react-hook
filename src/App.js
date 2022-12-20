c28700becc48310ee2df38363c093b71edd17956
Marcus Rocha <sif.marcusvinicius@gmail.com>
2022-12-20 16:51:46 -0300

    hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const evenFn = () => {
+  console.log('h2 clicado');
+};
 
 function App() {
   const [reverse, setReverse] = useState(false);
@@ -14,6 +18,28 @@ function App() {
   const handleSetCounter = () => {
     setCounter((counter) => counter + 1);
   };
+
+  // componentDidUpdate - executa toda vez que o component atualiza
+  useEffect(() => {
+    console.log('componentDidUpdate');
+  });
+
+  // componentDidMount - executa 1x
+  useEffect(() => {
+    //console.log('componentDidMount');
+    document.querySelector('h2')?.addEventListener('click', evenFn);
+
+    //componetWillUmount - Limpeza
+    return () => {
+      console.log('componetWillMount');
+      document.querySelector('h2')?.removeEventListener('click', evenFn);
+    };
+  }, []);
+
+  // com dependencia
+  useEffect(() => {
+    console.log('contador mudou para ', counter);
+  }, [counter]);
 
   return (
     <div className="App">
