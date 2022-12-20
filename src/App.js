e2667b3f9b98726e815f108f673b124ea9e1ffdf
Marcus Rocha <sif.marcusvinicius@gmail.com>
2022-12-20 16:00:49 -0300

    change App.js to use hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const reverseClass = reverse ? 'reverse' : 'App-logo';
+
+  const handleClick = () => {
+    setReverse(!reverse);
+  };
+
+  const handleSetCounter = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <img src={logo} className={reverseClass} alt="logo" />
+        <h2 onClick={handleSetCounter}>Contador: {counter}</h2>
+
+        <button onClick={handleClick}>Change Rotation</button>
       </header>
     </div>
   );
